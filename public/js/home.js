86b044efcfaee50828ec02e16994b3ef733abaa8
helbertsamuel <helbertsamuel@gmail.com>
2020-06-07 13:57:32 -0300

    commit inicial

--- public/js/home.js
+++ public/js/home.js
@@ -9,49 +9,37 @@ angular.module('app').controller('home', function ($scope, $http) {
 	    url: 'api/users',
 	    data: $scope.form
 	}).then(function successCallback(response) {
-	    if (response.data.status == 1) {
-		$('.div-ajax-carregamento-pagina').css('display', 'none');
-		return window.open('../../' + response.data.url, "Preview", 'height=768,width=1366,left=10,top=10,titlebar=no,toolbar=no,menubar=no,location=no,directories=no,status=no');
-		$scope.ajax = 0;
-	    } else {
-
-		$.gritter.add({
-		    title: 'Atenção',
-		    text: response.data.erro,
-		    time: 10000,
-		    class_name: 'erro'});
-		$scope.ajax = 0;
-	    }
-	    $('.div-ajax-carregamento-pagina').css('display', 'none');
+	    alert('Cadastrado com Sucesso!');
 	}, function errorCallback(response) {
-	    $scope.ajax = 0;
+
 	    if (response.status == 422) {
-		$("label").css("color", "#76838f");
-		var erros = '';
-		for (var i in response.data.errors) {
-		    for (var j in response.data.errors[i]) {
-			erros += response.data.errors[i][j] + '<br>';
-			var textU = $(response.data.errors[i][j]).wrap("<u></u>")[0];
-			if (textU) {
-			    var textFor = $(response.data.errors[i][j]).wrap("<u></u>")[0].innerHTML.trim();
-			    var label = $('label[for="' + textFor + '"]');
-			    $(label).css("color", "#FF0000");
-			}
-		    }
-		}
-		if (erros) {
-		    $.gritter.add({title: 'Atenção',
-			text: erros,
-			time: 10000,
-			class_name: 'erro'
-		    });
-		} else {
-		    alert('Não foi possível salvar. Por favor, tente novamente.');
-		}
+		alert('Erro na validação dos dados!')
 	    } else {
 		alert('Não foi possível prosseguir. Por favor, tente novamente.');
 	    }
-	    $('.div-ajax-carregamento-pagina').css('display', 'none');
+
 	});
     };
+    
+    listar();
+
+    function listar() {
+
+	$http({
+	    method: 'GET',
+	    url: 'api/users'
+	}).then(function successCallback(response) {
+	    console.log(response.data);
+	   $scope.cadastros = response.data;
+	}, function errorCallback(response) {
+
+	    if (response.status == 422) {
+		alert('Erro na validação dos dados!')
+	    } else {
+		alert('Não foi possível prosseguir. Por favor, tente novamente.');
+	    }
+
+	});
+    }
+    ;
 });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,14 +4,13 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="robots" content="none" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-	<script src="http://localhost/blog/public/js/angular.min.js"></script>	
-	<link rel="stylesheet" href="http://localhost/blog/public/css/app.css">
+	<script src="http://localhost/testePraticoIn8/public/js/angular.min.js"></script>	
+	<link rel="stylesheet" href="http://localhost/testePraticoIn8/public/css/app.css">
 	<script>
-	    (function () {
-		app = angular.module('app', []);
-	    })();
-	</script>
-	<script src="http://localhost/blog/public/js/home.js"></script>
+		    (function () {
+		    app = angular.module('app', []);
+		    })();</script>
+	<script src="http://localhost/testePraticoIn8/public/js/home.js"></script>
     </header>
 
     <body>
@@ -23,10 +22,37 @@
 			{!! Form::email('email' ,null, ['class'=>'form-control', 'placeholder'=>'email', 'id'=>'email', 'ng-model'=>'form.email']) !!}
 			{!! Form::text('nascimento' ,null, ['class'=>'form-control', 'placeholder'=>'Nascimento', 'id'=>'nascimento', 'ng-model'=>'form.nascimento']) !!}
 			{!! Form::text('telefone' ,null, ['class'=>'form-control', 'placeholder'=>'telefone', 'id'=>'telefone', 'ng-model'=>'form.telefone']) !!}
-			<input type="hidden" name="_token" value="{{ csrf_token() }}">
+			<input type="hidden" name="_token" value="{{ csrf_token()}}">
 			{!! Form::submit('Cadastrar')!!}
 		    </form>
                 </div>
+
+		<table class="table table-condensed table-striped table-hover odd" id="table-search" ng-init="cadastros">
+		    <thead>
+			<tr>
+			    <th>Nome</th>
+			    <th>Email</th>
+			    <th>Nacimento</th>
+			    <th>Telefone</th>
+			</tr>
+		    </thead>
+		    <tbody>
+			<tr ng-repeat="item in cadastros">
+			    <td class="readmore">
+				<input type="text" ng-model="item.nome">
+			    </td>
+			    <td class="readmore">
+				<input type="text" ng-model="item.email">
+			    </td>
+			    <td class="readmore">
+				<input type="text" ng-model="item.nascimento">
+			    </td>
+			    <td class="readmore">
+				<input type="text" ng-model="item.telefone">
+			    </td>
+			</tr>
+		    </tbody>
+		</table>
             </div>
         </div>
     </body>
