1ea1a0835062f84e97e99576ad0f2460a6b7534e
helbertsamuel <helbertsamuel@gmail.com>
2020-06-07 16:43:19 -0300

    seed e migrations

--- database/seeds/CadastrosTableSeeder.php
+++ database/seeds/CadastrosTableSeeder.php
@@ -12,10 +12,18 @@ class CadastrosTableSeeder extends Seeder {
      */
     public function run() {
 
+	DB::table('cadastros')->delete();
+
 	Cadastro::create([
 	    'nome' => 'Helbert Samuel',
 	    'email' => 'helbertsamuel@gmail.com',
 	    'nascimento' => '22/05/1990',
+	    'telefone' => '31988291178',
+	]);
+	Cadastro::create([
+	    'nome' => 'Karine Moreira',
+	    'email' => 'karine@gmail.com.com',
+	    'nascimento' => '22/05/1995',
 	    'telefone' => '31988291178',
 	]);
     }
