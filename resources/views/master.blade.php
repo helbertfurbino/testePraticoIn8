48acb56437babad2f0e6084165d8640a9ad36b09
helbertsamuel <helbertsamuel@gmail.com>
2020-06-07 20:52:09 -0300

    tratamento api

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,4 +1,5 @@
 <header>
+    <title>Cadastro</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" charset="UTF-8">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
